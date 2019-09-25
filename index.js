9a017432cde67dde4bcadbe2eb781bdff0619605
Felix Dulfer <fdulfer@intothesource.com>
2019-09-25 20:42:14 +0200

    Add generator for sitemap index

--- index.js
+++ index.js
@@ -3,70 +3,73 @@
 const xml = require('xml');
 const createUrl = require('./lib/util/create-url');
 
+const isString = str =>
+	typeof str === 'string';
+
+const withUrl = key =>
+	base =>
+		loc => isString(loc) && {[key]: createUrl(loc, base)};
+
+const withString = key =>
+	str => str && {[key]: str};
+
+const withFloat = key =>
+	num => num && {[key]: num.toFixed(1)};
+
 class Sitemap {
-	constructor(urls = [], {base = ''} = {}) {
-		this.urls = urls;
-		this.base = base;
-	}
+	constructor(urls, options = {}) {
+		this.urls = urls || [];
+		this.base = options.base || '';
 
-	generate(urls = this.urls || [], base = this.base || '') {
-		const sitemap = {
-			urlset: {
-				url: []
-			}
-		};
-
-		urls.forEach(({loc, lastmod, changefreq, priority}) => {
-			const obj = {loc: createUrl(loc, base)};
-
-			if (lastmod) {
-				Object.assign(obj, {lastmod});
-			}
-
-			if (changefreq) {
-				Object.assign(obj, {changefreq});
-			}
-
-			if (priority) {
-				Object.assign(obj, {priority});
-			}
-
-			sitemap.urlset.url.push(obj);
-		});
-
-		return sitemap;
-	}
-
-	toObject() {
-		return this.generate(this.urls, this.base);
-	}
-
-	toJson({indent = false} = {}) {
-		return JSON.stringify(this.toObject(), null, indent ? 4 : 0);
-	}
-
-	toXml({indent = false} = {}) {
-		const {urlset} = this.toObject();
-		const {url} = urlset;
-		const xmlObj = [{
+		const model = [{
 			urlset: [
 				{_attr: {xmlns: 'http://www.sitemaps.org/schemas/sitemap/0.9'}},
-				...(url.length > 0 ?
-					url.map(({loc, lastmod, changefreq, priority}) => {
-						return {
-							url: [
-								loc 		&& {loc},
-								lastmod 	&& {lastmod},
-								changefreq 	&& {changefreq},
-								priority 	&& {priority}
-							].filter(Boolean)
-						};
-					}) :
-					[])
+				...(this.urls.length > 0) ?
+					this.urls.map(({loc, lastmod, changefreq, priority}) => ({
+						url: [
+							withUrl('loc')(this.base)(loc),
+							withString('lastmod')(lastmod),
+							withString('changefreq')(changefreq),
+							withFloat('priority')(priority)
+						].filter(Boolean)
+					})) :
+					[]
 			]
 		}];
-		return xml(xmlObj, {indent, declaration: {encoding: 'UTF-8'}});
+
+		this.model = model;
+	}
+
+	toString({indent = false} = {}) {
+		return xml(this.model, {indent, declaration: {encoding: 'UTF-8'}});
 	}
 }
 
-module.exports = Sitemap;
+class SitemapIndex {
+	constructor(urls, options = {}) {
+		this.urls = urls || [];
+		this.base = options.base || '';
+
+		const model = [{
+			sitemapindex: [
+				{_attr: {xmlns: 'http://www.sitemaps.org/schemas/sitemap/0.9'}},
+				...(this.urls.length > 0) ?
+					this.urls.map(({loc, lastmod}) => ({
+						sitemap: [
+							withUrl('loc')(this.base)(loc),
+							withString('lastmod')(lastmod)
+						].filter(Boolean)
+					})) :
+					[]
+			]
+		}];
+
+		this.model = model;
+	}
+
+	toString({indent = false} = {}) {
+		return xml(this.model, {indent, declaration: {encoding: 'UTF-8'}});
+	}
+}
+
+module.exports = {Sitemap, SitemapIndex};
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import castArray from './lib/util/cast-array';
 import createUrl from './lib/util/create-url';
-import Sitemap from '.';
+import {Sitemap, SitemapIndex} from '.';
 
 // ---
 // lib/util/cast-array
@@ -73,110 +73,34 @@ test('Sitemap#constructor() - with urls and base option', t => {
 });
 
 // ---
-// toObject
+// toString
 // ---
 
-test('Sitemap#toObject() - without urls or options', t => {
-	const sitemap = new Sitemap();
-	t.deepEqual(sitemap.toObject(), {
-		urlset: {
-			url: []
-		}
-	});
-});
-
-test('Sitemap#toObject() - with urls', t => {
-	const sitemap = new Sitemap([{loc: 'foo'}, {loc: 'bar'}]);
-	t.deepEqual(sitemap.toObject(), {
-		urlset: {
-			url: [{loc: 'foo'}, {loc: 'bar'}]
-		}
-	});
-});
-
-test('Sitemap#toObject() - with urls and base', t => {
-	const urls = [{loc: ''}, {loc: '/'}, {loc: 'foo'}, {loc: 'bar', priority: 1.0}];
-	const options = {base: 'https://example.com'};
-	const sitemap = new Sitemap(urls, options);
-	sitemap.base = 'https://example.com';
-	t.deepEqual(sitemap.toObject(), {
-		urlset: {
-			url: [
-				{loc: 'https://example.com'},
-				{loc: 'https://example.com/'},
-				{loc: 'https://example.com/foo'},
-				{loc: 'https://example.com/bar', priority: 1.0}
-			]
-		}
-	});
-});
-
-// ---
-// toJson
-// ---
-
-test('Sitemap#toJson() - without urls or options', t => {
+test('Sitemap#toString() - without urls or options', t => {
 	const sitemap = new Sitemap();
 	t.is(
-		sitemap.toJson(),
-		'{"urlset":{"url":[]}}'
-	);
-});
-
-test('Sitemap#toJson() - with urls', t => {
-	const sitemap = new Sitemap([{loc: 'foo'}, {loc: 'bar'}]);
-	t.is(
-		sitemap.toJson(),
-		'{"urlset":{"url":[{"loc":"foo"},{"loc":"bar"}]}}'
-	);
-});
-
-test('Sitemap#toJson() - with urls and base', t => {
-	const urls = [{loc: ''}, {loc: '/'}, {loc: 'foo'}, {loc: 'bar', priority: 0.5}];
-	const options = {base: 'https://example.com'};
-	const sitemap = new Sitemap(urls, options);
-	sitemap.base = 'https://example.com';
-	t.is(
-		sitemap.toJson(),
-		'{"urlset":{"url":[' +
-		'{"loc":"https://example.com"},' +
-		'{"loc":"https://example.com/"},' +
-		'{"loc":"https://example.com/foo"},' +
-		'{"loc":"https://example.com/bar","priority":0.5}' +
-		']}}'
-	);
-});
-
-// ---
-// toXml
-// ---
-
-test('Sitemap#toXml() - without urls or options', t => {
-	const sitemap = new Sitemap();
-	t.is(
-		sitemap.toXml(),
+		sitemap.toString(),
 		'<?xml version="1.0" encoding="UTF-8"?>' +
 		'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>'
 	);
 });
 
-test('Sitemap#toXml() - with urls', t => {
+test('Sitemap#toString() - with urls', t => {
 	const sitemap = new Sitemap([{loc: 'foo'}, {loc: 'bar'}]);
 	t.is(
-		sitemap.toXml(),
+		sitemap.toString(),
 		'<?xml version="1.0" encoding="UTF-8"?>' +
 		'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' +
 		'<url><loc>foo</loc></url><url><loc>bar</loc></url></urlset>'
 	);
 });
 
-test('Sitemap#toXml() - with urls and base', t => {
+test('Sitemap#toString() - with urls and base', t => {
 	const urls = [{loc: ''}, {loc: '/'}, {loc: 'foo'}, {loc: 'bar', priority: 0.5}];
 	const options = {base: 'https://example.com'};
 	const sitemap = new Sitemap(urls, options);
-	sitemap.base = 'https://example.com';
 	t.is(
-		sitemap.toXml(),
+		sitemap.toString(),
 		'<?xml version="1.0" encoding="UTF-8"?>' +
 		'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' +
 		'<url><loc>https://example.com</loc></url>' +
@@ -184,5 +108,65 @@ test('Sitemap#toXml() - with urls and base', t => {
 		'<url><loc>https://example.com/foo</loc></url>' +
 		'<url><loc>https://example.com/bar</loc><priority>0.5</priority></url>' +
 		'</urlset>'
+	);
+});
+
+// ---
+// Sitemap#constructor
+// ---
+
+test('SitemapIndex#constructor() - without urls or options', t => {
+	t.deepEqual(new SitemapIndex().urls, []);
+	t.deepEqual(new SitemapIndex().base, '');
+});
+
+test('SitemapIndex#constructor() - with urls', t => {
+	t.deepEqual(new SitemapIndex([{loc: 'foo'}]).urls, [{loc: 'foo'}]);
+});
+
+test('SitemapIndex#constructor() - with urls and base option', t => {
+	const urls = [{loc: 'foo'}];
+	const options = {base: 'https://example.com'};
+	const sitemap = new SitemapIndex(urls, options);
+	t.deepEqual(sitemap.urls, [{loc: 'foo'}]);
+	t.is(sitemap.base, 'https://example.com');
+});
+
+// ---
+// toString
+// ---
+
+test('SitemapIndex#toString() - without urls or options', t => {
+	const sitemap = new SitemapIndex();
+	t.is(
+		sitemap.toString(),
+		'<?xml version="1.0" encoding="UTF-8"?>' +
+		'<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></sitemapindex>'
+	);
+});
+
+test('SitemapIndex#toString() - with urls', t => {
+	const sitemap = new SitemapIndex([{loc: 'foo'}, {loc: 'bar'}]);
+	t.is(
+		sitemap.toString(),
+		'<?xml version="1.0" encoding="UTF-8"?>' +
+		'<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' +
+		'<sitemap><loc>foo</loc></sitemap><sitemap><loc>bar</loc></sitemap></sitemapindex>'
+	);
+});
+
+test('SitemapIndex#toString() - with urls and base', t => {
+	const urls = [{loc: ''}, {loc: '/'}, {loc: 'foo'}, {loc: 'bar', lastmod: '1985-10-25'}];
+	const options = {base: 'https://example.com'};
+	const sitemap = new SitemapIndex(urls, options);
+	t.is(
+		sitemap.toString(),
+		'<?xml version="1.0" encoding="UTF-8"?>' +
+		'<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' +
+		'<sitemap><loc>https://example.com</loc></sitemap>' +
+		'<sitemap><loc>https://example.com/</loc></sitemap>' +
+		'<sitemap><loc>https://example.com/foo</loc></sitemap>' +
+		'<sitemap><loc>https://example.com/bar</loc><lastmod>1985-10-25</lastmod></sitemap>' +
+		'</sitemapindex>'
 	);
 });
